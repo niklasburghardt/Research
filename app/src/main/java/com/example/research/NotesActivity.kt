c2e4cf7e34ac737cfbdc3330c1922332bde7143a
niklasburghardt <73792905+niklasburghardt@users.noreply.github.com>
2021-07-14 14:37:57 +0200

    notizen hinzufügen

--- app/src/main/java/com/example/research/NotesActivity.kt
+++ app/src/main/java/com/example/research/NotesActivity.kt
@@ -1,11 +1,19 @@
 package com.example.research
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class NotesActivity : AppCompatActivity() {
+    private lateinit var notes: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
+
+        notes = findViewById(R.id.edit_notes_text)
+        notes.movementMethod = ScrollingMovementMethod()
     }
 }
